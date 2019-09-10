690f2bfee351e531e1d90b53badd7a18e5937e75
Jay Monga <48458961+jaymonga@users.noreply.github.com>
2019-09-10 08:36:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,53 @@ var filterTodos = function filterTodos(term) {
   }).forEach(function (todo) {
     return todo.classList.add('filtered');
   });
-  Array.from(list.children).filter(function (todo) {
+  [].slice.call(list.children).filter(function (todo) {
+    return todo.textContent.toLowerCase().includes(term);
+  }).forEach(function (todo) {
+    return todo.classList.remove('filtered');
+  });
+}; //search keyup addEventListener
+
+
+search.addEventListener('keyup', function (e) {
+  var term = search.value.trim().toLowerCase();
+  filterTodos(term);
+});
+"use strict";
+
+var addForm = document.querySelector('.add');
+var list = document.querySelector('.todos');
+var search = document.querySelector('.search input');
+
+var generateTemplate = function generateTemplate(todo) {
+  var html = "<li class=\"list-group-item d-flex justify-content-between align-items-center\">\n                    <span>".concat(todo, "</span>\n                    <i class=\"far fa-trash-alt delete\"></i>\n                  </li>");
+  list.innerHTML += html;
+};
+
+addForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  var todo = addForm.add.value.trim();
+  console.log(todo);
+
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset();
+  }
+}); //delete todos
+
+list.addEventListener('click', function (e) {
+  if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
+  }
+});
+
+var filterTodos = function filterTodos(term) {
+  [].slice.call(list.children).filter(function (todo) {
+    return !todo.textContent.toLowerCase().includes(term);
+  }).forEach(function (todo) {
+    return todo.classList.add('filtered');
+  });
+  [].slice.call(list.children).filter(function (todo) {
     return todo.textContent.toLowerCase().includes(term);
   }).forEach(function (todo) {
     return todo.classList.remove('filtered');
