05d80bf034bfd72757528b34606cfd892d9c6191
Jay Monga <48458961+jaymonga@users.noreply.github.com>
2019-10-01 07:39:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-
 var addForm = document.querySelector('.add');
 var list = document.querySelector('.todos');
 var search = document.querySelector('.search input');
 
 var generateTemplate = function generateTemplate(todo) {
-  var html = "<li class=\"list-group-item d-flex justify-content-between align-items-center\">\n                    <span>".concat(todo, "</span>\n                    <i class=\"far fa-trash-alt delete\"></i>\n                  </li>");
+  var html = "<li class=\"list-group-item d-flex justify-content-between align-items-center\">\n                    <span>".concat(todo, "</span>\n                    <i class=\"delete\"></i>\n                  </li>");
   list.innerHTML += html;
 };
 
@@ -15,13 +14,25 @@ addForm.addEventListener('submit', function (e) {
 
   if (todo.length) {
     generateTemplate(todo);
+    // store data in local storage
+    localStorage.setItem(todo, todo);
+    
     addForm.reset();
   }
+  
+      var select = document.querySelector('.all-Items');
+      var presetItem = select.querySelectorAll('.preset');
+      for(i = 0; i < presetItem.length; i++) {
+          presetItem[i].remove();
+        }
 }); //delete todos
 
 list.addEventListener('click', function (e) {
+  e.preventDefault();
   if (e.target.classList.contains('delete')) {
     e.target.parentElement.remove();
+    localStorage.removeItem(e.target.parentElement.innerText.trim());
+    console.log(e.target.parentElement.innerText.trim());
   }
 });
 
@@ -40,52 +51,23 @@ var filterTodos = function filterTodos(term) {
 
 
 search.addEventListener('keyup', function (e) {
-  var term = search.value.trim().toLowerCase();
-  filterTodos(term);
-});
-"use strict";
-
-var addForm = document.querySelector('.add');
-var list = document.querySelector('.todos');
-var search = document.querySelector('.search input');
-
-var generateTemplate = function generateTemplate(todo) {
-  var html = "<li class=\"list-group-item d-flex justify-content-between align-items-center\">\n                    <span>".concat(todo, "</span>\n                    <i class=\"far fa-trash-alt delete\"></i>\n                  </li>");
-  list.innerHTML += html;
-};
-
-addForm.addEventListener('submit', function (e) {
   e.preventDefault();
-  var todo = addForm.add.value.trim();
-  console.log(todo);
-
-  if (todo.length) {
-    generateTemplate(todo);
-    addForm.reset();
-  }
-}); //delete todos
-
-list.addEventListener('click', function (e) {
-  if (e.target.classList.contains('delete')) {
-    e.target.parentElement.remove();
-  }
-});
-
-var filterTodos = function filterTodos(term) {
-  [].slice.call(list.children).filter(function (todo) {
-    return !todo.textContent.toLowerCase().includes(term);
-  }).forEach(function (todo) {
-    return todo.classList.add('filtered');
-  });
-  [].slice.call(list.children).filter(function (todo) {
-    return todo.textContent.toLowerCase().includes(term);
-  }).forEach(function (todo) {
-    return todo.classList.remove('filtered');
-  });
-}; //search keyup addEventListener
-
-
-search.addEventListener('keyup', function (e) {
   var term = search.value.trim().toLowerCase();
   filterTodos(term);
 });
+
+window.onload = function() {
+  
+    // get data from local storage
+    if(localStorage.length>0){
+      var select = document.querySelector('.all-Items');
+      var presetItem = select.querySelectorAll('.preset');
+      for(i = 0; i < presetItem.length; i++) {
+          presetItem[i].remove();
+        }
+        for(var i =0; i < localStorage.length; i++){
+          var storageKey = localStorage.getItem(localStorage.key(i));
+          generateTemplate(storageKey)
+        }
+    }
+};
